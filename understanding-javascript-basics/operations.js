b06278d1f2ed743c3e0f034899e97b605da118d8
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-10 17:05:15 +0200

    Math operations and math rules

--- understanding-javascript-basics/operations.js
+++ understanding-javascript-basics/operations.js
@@ -3,3 +3,14 @@ console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4);
+
+let result = (10 + 3 - 5) * 10;
+
+result++;
+result--;
+
+result += 5;
+result -= 5;
+result /= 5;
+result *= 5;
+console.log(result);
