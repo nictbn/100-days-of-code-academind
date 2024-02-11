ed1d56a7ce7645572227d2925e88ad2cd595fb98
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-11 15:46:39 +0200

    Explores types of selectors

--- advanced-javascript-working-with-the-dom/app.js
+++ advanced-javascript-working-with-the-dom/app.js
@@ -1,2 +1,8 @@
 let anchorElement = document.getElementById("external-link");
-anchorElement.href="https://google.com";
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector("#external-link");
+anchorElement.href = "https://wikipedia.org"
+
+anchorElement = document.querySelector("p a");
+anchorElement.href = "https://wikipedia.org"
