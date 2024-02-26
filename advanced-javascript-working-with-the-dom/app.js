64559f3db75c53d0e3fa91c722fb6fdaf137bffe
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-26 23:09:52 +0200

    Inserts an element using javascript

--- advanced-javascript-working-with-the-dom/app.js
+++ advanced-javascript-working-with-the-dom/app.js
@@ -2,7 +2,14 @@ let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
 
 anchorElement = document.querySelector("#external-link");
-anchorElement.href = "https://wikipedia.org"
+anchorElement.href = "https://wikipedia.org";
 
 anchorElement = document.querySelector("p a");
-anchorElement.href = "https://wikipedia.org"
+anchorElement.href = "https://wikipedia.org";
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href='https://google.com';
+newAnchorElement.textContent='This leads to google';
+
+let firstParagraph = document.querySelector('p');
+firstParagraph.append(newAnchorElement);
