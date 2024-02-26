19364979149abd39291c484104b0a545dccc8ff7
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-26 23:38:37 +0200

    Add an event listener

--- advanced-javascript-working-with-the-dom/app.js
+++ advanced-javascript-working-with-the-dom/app.js
@@ -2,6 +2,7 @@ let paragraphElement = document.querySelector('p');
 
 function changeParagraphText() {
     paragraphElement.textContent = 'Clicked!';
+    console.log('Paragraph clicked!');
 }
 
 paragraphElement.addEventListener('click', changeParagraphText);
