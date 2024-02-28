38ea0cc45308d03fd23684f0649711262bc34133
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-28 11:57:51 +0200

    Use const instead of let

--- advanced-javascript-working-with-the-dom/demo.js
+++ advanced-javascript-working-with-the-dom/demo.js
@@ -1,11 +1,11 @@
-let productNameInputElement = document.getElementById('product-name');
-let remainingCharsElement = document.getElementById('remaining-chars');
-let maxAllowedChars = productNameInputElement.maxLength;
+const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
     remainingCharsElement.textContent = remainingCharacters;
 }
 
