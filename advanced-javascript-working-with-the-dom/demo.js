bd624207fd27533153b797d181b2438a794b1607
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-28 17:23:22 +0200

    intro to if else statements

--- advanced-javascript-working-with-the-dom/demo.js
+++ advanced-javascript-working-with-the-dom/demo.js
@@ -7,6 +7,20 @@ function updateRemainingCharacters(event) {
     const enteredTextLength = enteredText.length;
     const remainingCharacters = maxAllowedChars - enteredTextLength;
     remainingCharsElement.textContent = remainingCharacters;
+    if (remainingCharacters === 0) {
+        remainingCharsElement.classList.add('error');
+        productNameInputElement.classList.add('error');
+    } else if (remainingCharacters <= 10) {
+        remainingCharsElement.classList.add('warning');
+        productNameInputElement.classList.add('warning');
+        remainingCharsElement.classList.remove('error');
+        productNameInputElement.classList.remove('error');
+    } else {
+        remainingCharsElement.classList.remove('warning');
+        productNameInputElement.classList.remove('warning');
+        remainingCharsElement.classList.remove('error');
+        productNameInputElement.classList.remove('error');
+    }
 }
 
 productNameInputElement.addEventListener('input', updateRemainingCharacters);
