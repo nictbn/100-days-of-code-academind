cfdf08ddc88f1c1d00fc9edf5f64122c769869aa
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-28 17:57:42 +0200

    truthy and falsy variables

--- advanced-javascript-working-with-the-dom/conditional-code.js
+++ advanced-javascript-working-with-the-dom/conditional-code.js
@@ -9,3 +9,12 @@ const isLoggedIn = false;
 if (!isLoggedIn) {
     console.log('User is not logged in!');
 }
+
+const enteredUserName = 'Maxumilian';
+// if (enteredUserName.length > 0) {
+//     console.log('Input is valid!')
+// }
+
+if (enteredUserName) {
+    console.log('Input is valid!')
+}
