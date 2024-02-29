510fa820c5d65d1b10ef37ed0739228dcccf860b
Nicu Taban <nictbndeveloper@gmail.com>
2024-02-29 23:05:28 +0200

    Manages game rounds

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,8 +1,10 @@
 let editedPlayer = 0;
+let activePlayer = 0;
+
 const players = [
     {
         name: '',
-        Symbol: 'X'
+        symbol: 'X'
     },
     {
         name: '',
@@ -15,11 +17,15 @@ const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const startNewGameBtnElement = document.getElementById('start-game-btn');
+
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.querySelector('#game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -28,3 +34,8 @@ backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//     gameFieldElement.addEventListener('click', selectGameField);
+// }
+gameBoardElement.addEventListener('click', selectGameField);
