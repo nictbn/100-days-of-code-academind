dd35e9673504897f7a065c53a7fece99537be95d
Nicu Taban <nictbndeveloper@gmail.com>
2024-03-09 18:33:05 +0200

    Adds function to check for winner

--- tic-tac-toe/scripts/game.js
+++ tic-tac-toe/scripts/game.js
@@ -7,6 +7,40 @@ function startNewGame() {
     gameAreaElement.style.display = 'block';
 }
 
+function checkForGameOver() {
+    for (let i = 0; i < 3; i++) {
+        if (gameData[i][0] > 0 &&
+            gameData[i][0] === gameData[i][1] && 
+            gameData[i][1] === gameData[i][2]
+        ) {
+            return gameData[i][0];
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (gameData[0][i] > 0 &&
+            gameData[0][i] === gameData[1][i] && 
+            gameData[0][i] === gameData[2][i]
+        ) {
+            return gameData[i][0];
+        }
+    }
+
+    if (gameData[0][0] > 0 && gameData[0][0] === gameData[1][1] && gameData[1][1] === gameData[2][2]) {
+        return gameData[0][0];
+    }
+
+    if (gameData[2][0] > 0 && gameData[2][0] === gameData[1][1] && gameData[1][1] === gameData[0][2]) {
+        return gameData[2][0];
+    }
+
+    if (currentRound === 9) {
+        return -1;
+    }
+
+    return 0;
+}
+
 function switchPlayer() {
     if (activePlayer === 0) {
         activePlayer = 1;
@@ -35,5 +69,7 @@ function selectGameField(event) {
     
 
     gameData[selectedRow][selectedColumn] = activePlayer + 1;
+    const winnerId = checkForGameOver();
+    currentRound++;
     switchPlayer();
 }
