734ac512114541765efd6ba6e2a5c3848cb06fd8
Nicu Taban <nictbndeveloper@gmail.com>
2024-03-31 15:41:46 +0300

    Install the ejs templating engine

--- static-and-dynamic-content-with-templates/app.js
+++ static-and-dynamic-content-with-templates/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: false}));
