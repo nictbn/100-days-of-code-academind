20c9624a1c1be628272e53d761de8efdbdda8bc1
Nicu Taban <nictbndeveloper@gmail.com>
2024-03-31 15:54:12 +0300

    Exemplifies adding dynamic content to a page

--- static-and-dynamic-content-with-templates/app.js
+++ static-and-dynamic-content-with-templates/app.js
@@ -16,7 +16,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/restaurants', function(req, res) {
-    res.render('restaurants');
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+    let templateVariables = {
+        numberOfRestaurants: storedRestaurants.length
+    };
+    res.render('restaurants', templateVariables);
 });
 
 app.get('/recommend', function(req, res) {
