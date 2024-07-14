259a14ffca7d8d733ff18db3785fa32e7c9a45b9
Nicu Taban <nictbndeveloper@gmail.com>
2024-07-14 16:03:19 +0300

    Implements the update post functionality

--- mongodb-in-nodejs/routes/blog.js
+++ mongodb-in-nodejs/routes/blog.js
@@ -53,6 +53,28 @@ router.get('/posts/:id', async function(req, res) {
   });
   post.date = post.date.toISOString();
   res.render('post-detail', { post: post });
-})
+});
 
+router.get('/posts/:id/edit', async function(req, res) {
+  const postId = req.params.id;
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)}, {title: 1, summary: 1, body: 1});
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+  res.render('update-post', { post: post });
+});
+
+router.post('/posts/:id/edit', async function(req, res) {
+  const postId = req.params.id;
+  await db.getDb().collection('posts').updateOne(
+    {_id: new ObjectId(postId)}, 
+    {$set: {
+      title: req.body.title,
+      summary: req.body.summary,
+      body: req.body.content
+    }});
+
+  res.redirect('/posts');
+});
 module.exports = router;
