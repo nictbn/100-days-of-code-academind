80d7c9c70a8248d2ec559fcd84e37e06ad90d42e
Nicu Taban <nictbndeveloper@gmail.com>
2024-07-15 22:29:58 +0300

    Stores the image path in the db

--- adding-a-file-upload-to-a-website/routes/users.js
+++ adding-a-file-upload-to-a-website/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const db = require('../data/database');
 
 const storageConfig = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -21,11 +22,13 @@ router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image'), function(req, res) {
+router.post('/profiles', upload.single('image'), async function(req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
-  console.log(uploadedImageFile);
-  console.log(userData);
+  await db.getDb().collection('users').insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path
+  });
   res.redirect('/');
 });
 
