ecdc4234c4ca1f9dab2832cb0471a6202ae6c780
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-03 18:30:10 +0300

    Parses the comments and displays them on screen

--- ajax-intro/public/scripts/comments.js
+++ ajax-intro/public/scripts/comments.js
@@ -1,10 +1,29 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsSectionElement = document.getElementById('comments');
+
+function createCommentsList(comments) {
+    const commentListElement = document.createElement('ol');
+    for (const comment of comments) {
+        const commentElement = document.createElement('li');
+        commentElement.innerHTML = `
+        <article class="comment-item">
+          <h2>${comment.title}</h2>
+          <p>${comment.text}</p>
+        </article>
+        `;
+        commentListElement.appendChild(commentElement);
+    }
+    return commentListElement;
+}
 
 async function fetchCommentsForPost() {
     const postid = loadCommentsBtnElement.dataset.postid
     const response = await fetch(`/posts/${postid}/comments`);
     const responseData = await response.json();
-    console.log(responseData)
+    const commentsListElement = createCommentsList(responseData);
+    console.log(commentsListElement);
+    commentsSectionElement.innerHTML = '';
+    commentsSectionElement.appendChild(commentsListElement);
 }
 
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
