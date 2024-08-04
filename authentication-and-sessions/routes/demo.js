07cb53df1ae9a265eef710f75036ec203d86906a
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-04 14:39:41 +0300

    Example of using sessions for validation purposes

--- authentication-and-sessions/routes/demo.js
+++ authentication-and-sessions/routes/demo.js
@@ -10,7 +10,17 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  let sessionInputData = req.session.inputData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: ''
+    }
+  }
+  req.session.inputData = null;
+  res.render('signup', {inputData: sessionInputData});
 });
 
 router.get('/login', function (req, res) {
@@ -25,7 +35,17 @@ router.post('/signup', async function (req, res) {
 
   if (!enteredEmail || !enteredConfirmEmail || !enteredPassword || enteredPassword.trim() < 6 || enteredEmail !== enteredConfirmEmail) {
     console.log('Incorrect data')
-    return res.redirect('/signup')
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - please check your data',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+    req.session.save(function() {
+      res.redirect('/signup');
+    });
+    return
   }
 
   const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail});
