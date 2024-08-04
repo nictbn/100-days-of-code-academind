9948eed9ec32e2d4e2866adc789b68571a6fc421
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-04 11:04:37 +0300

    First implementation of the signup endpoint

--- authentication-and-sessions/routes/demo.js
+++ authentication-and-sessions/routes/demo.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const db = require('../data/database');
 
@@ -16,7 +17,23 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData['confirm-email'];
+  const enteredPassword = userData.password;
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword
+  }
+
+  await db.getDb().collection('users').insertOne(user);
+
+  res.redirect('/login');
+});
 
 router.post('/login', async function (req, res) {});
 
