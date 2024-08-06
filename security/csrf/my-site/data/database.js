0adc0695aea929c0fe9bfd3f8b861169d2d7a3c3
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-06 23:56:55 +0300

    Configures db for the mongo demos

--- security/csrf/my-site/data/database.js
+++ security/csrf/my-site/data/database.js
@@ -6,7 +6,7 @@ let database;
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(
-    'mongodb://localhost:27017'
+    'mongodb://topsecretuser:topsecretpass@127.0.0.1:27017/'
   );
   database = client.db('security');
 }
