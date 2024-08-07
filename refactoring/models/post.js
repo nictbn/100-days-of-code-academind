564307955eae1e7b6b244378be9841c0ce7fd5f1
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-07 23:49:20 +0300

    Adds update and delete functionalities to the post model

--- refactoring/models/post.js
+++ refactoring/models/post.js
@@ -1,17 +1,38 @@
+const { ObjectId } = require('mongodb');
 const db = require('../data/database');
 class Post {
     constructor(title, content, id) {
         this.title = title;
         this.content = content;
-        this.id = id;
+        if (id) {
+            this.id = new ObjectId(id)
+        }
     }
 
-    async save() {
-        const result = await db.getDb().collection('posts').insertOne({
-            title: this.title,
-            content: this.content,
-        });
+    async upsert() {
+        let result;
+        if (this.id) {
+            result = await db
+                .getDb()
+                .collection('posts')
+                .updateOne(
+                            { _id: this.id },
+                            { $set: { title: this.title, content: this.content } }
+                        );
+        } else {
+            result = await db.getDb().collection('posts').insertOne({
+                title: this.title,
+                content: this.content,
+            });
+        }        
         return result;
+    }
+
+    async delete() {
+        if (!this.id) {
+            return;
+        }
+        return await db.getDb().collection('posts').deleteOne({ _id: this.id });
     }
 }
 
