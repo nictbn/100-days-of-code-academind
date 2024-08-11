ffa6b7401af6cab6b1b09e6e58506b818c7907c0
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-11 20:01:29 +0300

    Adds csrf protection but it does not work because we don't support sessions yet

--- capstone/app.js
+++ capstone/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
+const csrf = require('csurf');
+
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-tokens');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -9,6 +12,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded( {extended: false } ));
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
 
