c89d08b0051a8b87b834ed6ea9e5933ce781cc97
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-11 22:55:50 +0300

    Handles errors in asynchronous code

--- capstone/controllers/auth.controller.js
+++ capstone/controllers/auth.controller.js
@@ -6,9 +6,15 @@ function getSignup(req, res) {
     res.render('customer/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
     const user = new User(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city);
-    await user.signup();
+
+    try {
+        await user.signup();
+    } catch(error) {
+        next(error);
+        return;
+    }
     res.redirect('/login');
 }
 
@@ -16,9 +22,16 @@ function getLogin(req, res) {
     res.render('customer/auth/login');
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
     const user = new User(req.body.email, req.body.password);
-    const existingUser = await user.getUserWithSameEmail();
+    let existingUser;
+    try {
+        existingUser = await user.getUserWithSameEmail();
+    } catch(error) {
+        next(error);
+        return;
+    }
+    
 
     if (!existingUser) {
         res.redirect('/login');
