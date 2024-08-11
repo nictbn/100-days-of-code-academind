6d4100d2c7d8cf93742610b8bc8096188bb8fc0c
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-11 20:49:25 +0300

    Adds session handling

--- capstone/app.js
+++ capstone/app.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-tokens');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
@@ -13,6 +15,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded( {extended: false } ));
+
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig));
+
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
