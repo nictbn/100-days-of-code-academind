654a2f0ae482d373a34703d192c25d3d89ce0f41
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-11 19:51:38 +0300

    Initial implementation of the signup functionality

--- capstone/app.js
+++ capstone/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
+app.use(express.urlencoded( {extended: false } ));
 
 app.use(authRoutes);
 
