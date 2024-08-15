217873c7b055e3885f1aeb4906de1201a60f514e
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-15 16:52:35 +0300

    Adds product admin pages

--- capstone/app.js
+++ capstone/app.js
@@ -12,7 +12,8 @@ const User = require('./models/user.model');
 
 const authRoutes = require('./routes/auth.routes');
 const productRoutes = require('./routes/products.routes');
-const baseRoutes = require('./routes/base.routes')
+const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 const app = express();
 
@@ -31,6 +32,8 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productRoutes);
+app.use('/admin', adminRoutes);
+
 
 app.use(errorHandlerMiddleware);
 db.connectToDatabase().then(User.createDefaultAdminIfMissing).then(function() {
