08524007a289bc1b63168aeada53c8df5f96f878
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-17 22:37:42 +0300

    Populates the update product form

--- capstone/models/product.model.js
+++ capstone/models/product.model.js
@@ -1,4 +1,5 @@
 const db = require('../data/database');
+const mongodb = require('mongodb');
 
 class Product {
     
@@ -9,12 +10,31 @@ class Product {
         });
     }
 
+    static async findById(productId) {
+        let prodId;
+        try {
+            prodId = new mongodb.ObjectId(productId);
+        } catch (error) {
+            error.code = 404;
+            throw error;
+        }
+    
+        const productDocument = await db.getDb().collection('products').findOne({_id: prodId});
+        if (!productDocument) {
+            const error = new Error('Could not find product with provided id.');
+            error.code = 404;
+            throw error;
+        }
+        const product = new Product(productDocument);
+        return product;
+    }
+
     constructor(productData) {
         this.title = productData.title;
         this.summary = productData.summary;
         this.price = +productData.price;
-        this.description = productData.description; // more specifically, the name of the image
-        this.image = productData.image;
+        this.description = productData.description; 
+        this.image = productData.image; // more specifically, the name of the image
         this.imagePath = `product-data/images/${productData.image}`;
         this.imageUrl = `/products/assets/images/${productData.image}`;
         if (productData._id) {
