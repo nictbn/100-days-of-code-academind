a25306bc901b81775d4697b559bf86f59a44b5d9
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-18 01:04:14 +0300

    Adds a 404 page

--- capstone/controllers/admin.controller.js
+++ capstone/controllers/admin.controller.js
@@ -31,7 +31,7 @@ async function createNewProduct(req, res, next) {
     res.redirect('/admin/products');
 }
 
-async function getUpdateProduct(req, res) {
+async function getUpdateProduct(req, res, next) {
     try {
         const product = await Product.findById(req.params.id);
         res.render('admin/products/update-product', { product: product });
