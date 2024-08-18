09dea44478d97f49a14794df72b9cc1b516eea50
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-18 16:42:21 +0300

    Adds patch route for updating cart items

--- capstone/controllers/cart.controller.js
+++ capstone/controllers/cart.controller.js
@@ -21,7 +21,22 @@ function getCart(req, res, next) {
     res.render('customer/cart/cart');
 }
 
+function updateCartItem(req, res, next) {
+    const cart = res.locals.cart;
+    const updatedItemData = cart.updateItem(req.body.productId, req.body.quantity);
+    req.session.cart = cart;
+    res.json({
+        message: 'Item updated!',
+        updatedCartData: {
+            newTotalQuantity: cart.totalQuantity,
+            newTotalPrice: cart.totalPrice,
+            updatedItemPrice: updatedItemData.updatedItemPrice,
+        }
+    })
+}
+
 module.exports = {
     addCartItem: addCartItem,
     getCart: getCart,
+    updateCartItem: updateCartItem,
 }
