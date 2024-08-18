6384db9b26876edc90dc2c9ff929f657e2b59d80
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-18 17:35:04 +0300

    Hadnles updating product quantities

--- capstone/models/cart.model.js
+++ capstone/models/cart.model.js
@@ -31,16 +31,16 @@ class Cart {
     updateItem(productId, newQuantity) {
         for (let i = 0; i < this.items.length; i++) {
             const item = this.items[i];
-            if (item.product.id === product.id && newQuantity > 0) {
+            if (item.product.id === productId && newQuantity > 0) {
                 const cartItem = { ...item };
                 const quantityChange = newQuantity - item.quantity;
                 cartItem.quantity = newQuantity
-                cartItem.totalPrice = newQuantity * product.price;
+                cartItem.totalPrice = newQuantity * item.product.price;
                 this.items[i] = cartItem;
                 this.totalQuantity = this.totalQuantity + quantityChange;
-                this.totalPrice += quantityChange * product.price;
+                this.totalPrice += quantityChange * item.product.price;
                 return { updatedItemPrice: cartItem.totalPrice };
-            } else if (item.product.id === product.id && newQuantity <= 0) {
+            } else if (item.product.id === productId && newQuantity <= 0) {
                 this.items.splice(i, 1);
                 this.totalQuantity = this.totalQuantity - item.quantity;
                 this.totalPrice -= item.totalPrice;
