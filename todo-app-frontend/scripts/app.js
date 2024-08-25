17df33ee12c48e4dfd146a79c68e91f69031cfa6
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-25 14:21:34 +0300

    Introduction to Vue

---
+++ todo-app-frontend/scripts/app.js
@@ -0,0 +1,16 @@
+const TodosApp = {
+    data() {
+        return {
+            newTodo: 'Learn Vue.js'
+        };
+    },
+
+    methods: {
+        saveTodo(event) {
+            event.preventDefault();
+            this.newTodo = 'Updated';
+        }
+    }
+};
+
+Vue.createApp(TodosApp).mount('#todos-app');
