42b3c027be265d7246cc733b077317e10d3b0e44
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-25 14:34:02 +0300

    Displays a list of todos using Vue

--- todo-app-frontend/scripts/app.js
+++ todo-app-frontend/scripts/app.js
@@ -1,15 +1,19 @@
 const TodosApp = {
     data() {
         return {
-            newTodo: 'Learn Vue.js',
-            enteredTodoText: 'New Todo...'
+            todos: [],
+            enteredTodoText: '',
         };
     },
 
     methods: {
         saveTodo(event) {
             event.preventDefault();
-            this.newTodo = this.enteredTodoText;
+            const newTodo = {
+                text: this.enteredTodoText,
+                id: new Date().toISOString()
+            };
+            this.todos.push(newTodo);
             this.enteredTodoText = '';
         }
     }
